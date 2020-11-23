8cb056e5a5ee306a09799fe73f0edee0068be92b
Sanmonsua <sanmonsua@gmail.com>
2020-11-22 22:38:51 -0500

    use environment variable to get the user agent

--- spider.py
+++ spider.py
@@ -1,5 +1,6 @@
 import scrapy
 import time
+import os
 
 
 class AmazonSpider(scrapy.Spider):
@@ -10,16 +11,15 @@ class AmazonSpider(scrapy.Spider):
         search_input = input('What are you looking for today? ')
         search = '+'.join(search_input.split(' '))
         url = f'{self.base_url}s?k={search}'
-        yield scrapy.Request(url=url, callback=self.parse, meta={ 'search' : search }, headers={ 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36'})
+        yield scrapy.Request(url=url, callback=self.parse, meta={ 'search' : search }, headers={ 'User-Agent' : os.environ['USER_AGENT']})
 
     def parse(self, response):
-        print(response.body)
         search = response.meta['search']
         pages_num = response.xpath('//ul[@class="a-pagination"]/li[last()-1]/text()').get()
         pages_urls = [ f'{self.base_url}s?k={search}&page={p}' for p in range(1, int(pages_num)+1)]
         for page in pages_urls:
             time.sleep(1)
-            yield scrapy.Request(url=page, callback=self.parse_search, headers={ 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36'})
+            yield scrapy.Request(url=page, callback=self.parse_search, headers={ 'User-Agent' : os.environ['USER_AGENT']})
         
 
     def parse_search(self, response):
